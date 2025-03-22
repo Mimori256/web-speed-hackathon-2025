6beb9e550aaf0e332e4b1d1fef9d9a4ab388e105
Mimori256 <mimorimori256@gmail.com>
2025-03-22 16:01:39 +0900

    server: use fastify compress

--- workspaces/server/src/index.ts
+++ workspaces/server/src/index.ts
@@ -2,6 +2,7 @@ import '@wsh-2025/server/src/setups/luxon';
 
 import cors from '@fastify/cors';
 import fastify from 'fastify';
+import compression from '@fastify/compress';
 
 import { registerApi } from '@wsh-2025/server/src/api';
 import { initializeDatabase } from '@wsh-2025/server/src/drizzle/database';
@@ -22,6 +23,7 @@ async function main() {
   app.register(registerApi, { prefix: '/api' });
   app.register(registerStreams);
   app.register(registerSsr);
+  app.register(compression);
 
   await app.ready();
   const address = await app.listen({ host: '0.0.0.0', port: Number(process.env['PORT']) });
