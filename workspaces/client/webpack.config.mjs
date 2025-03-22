c58cb7288c075a966638aa05ddb605fa42405a0a
Mimori256 <mimorimori256@gmail.com>
2025-03-22 11:27:13 +0900

    build: Remove core-js

--- workspaces/client/webpack.config.mjs
+++ workspaces/client/webpack.config.mjs
@@ -24,10 +24,7 @@ const config = {
               [
                 '@babel/preset-env',
                 {
-                  corejs: '3.41',
-                  forceAllTransforms: true,
                   targets: 'defaults',
-                  useBuiltIns: 'entry',
                 },
               ],
               ['@babel/preset-react', { runtime: 'automatic' }],
