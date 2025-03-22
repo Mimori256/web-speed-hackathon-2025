0da69f713563b0d60254bdd98ff27d8b2bde1427
Mimori256 <mimorimori256@gmail.com>
2025-03-22 15:32:50 +0900

    perf: defer loading script

--- workspaces/client/src/app/Document.tsx
+++ workspaces/client/src/app/Document.tsx
@@ -15,7 +15,7 @@ export const Document = () => {
       <head>
         <meta charSet="UTF-8" />
         <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-        <script src="/public/main.js"></script>
+        <script src="/public/main.js" defer></script>
       </head>
       <body className="size-full bg-[#000000] text-[#ffffff]">
         <Suspense>
