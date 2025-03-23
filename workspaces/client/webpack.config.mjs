fb294ac2a1b64413483059b49afc50c9e2a497e6
Mimori256 <mimorimori256@gmail.com>
2025-03-23 14:39:44 +0900

    Revert "fix: remove useBuiltin options"
    This reverts commit 5c4ef3a942949d4f0cf8ed2bab1b830c5c2ab468.

--- workspaces/client/webpack.config.mjs
+++ workspaces/client/webpack.config.mjs
@@ -26,7 +26,17 @@ const config = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [['@babel/preset-react', { runtime: 'automatic' }], ['@babel/preset-typescript']],
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: 'defaults',
+                  useBuiltIns: 'entry',
+                },
+              ],
+              ['@babel/preset-react', { runtime: 'automatic' }],
+              ['@babel/preset-typescript'],
+            ],
           },
         },
       },
